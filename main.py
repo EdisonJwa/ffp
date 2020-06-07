2bd752b36d995a991f04ddb276610ba1705aef66
EdisonJwa <i@uv.uy>
2020-06-07 10:45:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,24 @@ from mimetypes import MimeTypes
 import requests
 from flask import Flask, Response, abort, send_from_directory, redirect, request
 
+import base64
+
 app = Flask(__name__)
 app.debug = False
 mime = MimeTypes()
 CHUNK_SIZE = 2 * 1024 * 1024
 
-
 @app.route('/<path:url>')
 def proxy(url):
+
+    url_bytes = url.encode('ascii')
+    urldecode_bytes = base64.b64decode(url_bytes)
+    urldecode = urldecode_bytes.decode('ascii')
+    filename = urldecode.split("/")[-1]
+
     requestHeaders = dict(request.headers)
     # hostPattern=re.compile(r"\/\/(.*?)\/")
-    # requestHost=hostPattern.findall(url)[0]
+    # requestHost=hostPattern.findall(urldecode)[0]
     # requestHeaders['Host']=requestHost
     try:
         requestHeaders.pop('If-None-Match')
@@ -30,55 +37,22 @@ def proxy(url):
         requestHeaders.pop('Host')
     except:
         pass
-    r = requests.get(url, stream=True, params=request.args, headers=requestHeaders)
+    r = requests.get(urldecode, stream=True, params=request.args, headers=requestHeaders)
     if r.status_code != 200:
         abort(r.status_code)
-    mime_type = mime.guess_type(url)
+    mime_type = mime.guess_type(filename)
 
     def generate():
         for chunk in r.iter_content(CHUNK_SIZE):
             yield chunk
 
     responseHeaders = dict(r.headers)
+    responseHeaders['Content-Disposition'] = 'attachment; filename=' + filename
     try:
         responseHeaders.pop('Transfer-Encoding')
     except:
         pass
     return Response(generate(), headers=responseHeaders, mimetype=mime_type[0])
-
-
-@app.route('/r/<path:url>')
-def replace(url):
-    requestHeaders = dict(request.headers)
-    # hostPattern=re.compile(r"\/\/(.*?)\/")
-    # requestHost=hostPattern.findall(url)[0]
-    # requestHeaders['Host']=requestHost
-    try:
-        requestHeaders.pop('If-None-Match')
-    except:
-        pass
-    try:
-        requestHeaders.pop('If-Modified-Since')
-    except:
-        pass
-    try:
-        requestHeaders.pop('Host')
-    except:
-        pass
-    r = requests.get(url, params=request.args, headers=requestHeaders)
-    if r.status_code != 200:
-        abort(r.status_code)
-    mime_type = mime.guess_type(url)
-    localHost = request.headers["Host"]
-    pattern = re.compile(r'http(s:\/\/|:\/\/)')
-    postRegex = re.sub(pattern, "https://" + localHost + "/http" + r'\1', r.text)
-    responseHeaders = dict(r.headers)
-    try:
-        responseHeaders.pop('Content-Encoding')
-    except:
-        pass
-    return Response(postRegex, headers=responseHeaders, mimetype=mime_type[0])
-
 
 @app.route('/')
 def index():
